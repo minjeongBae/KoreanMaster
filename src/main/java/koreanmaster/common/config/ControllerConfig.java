194fb2a453515a9b27d864da65b79f16288b23b7
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-12-06 19:50:05 +0900

    feat: UI 및 페이지 이동을 위한 controller

--- src/main/java/koreanmaster/common/config/ControllerConfig.java
+++ src/main/java/koreanmaster/common/config/ControllerConfig.java
@@ -1,6 +1,8 @@
 package koreanmaster.common.config;
 
 import koreanmaster.board.controller.BoardController;
+import koreanmaster.home.controller.HomeController;
+import koreanmaster.mypage.controller.MyPageController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +11,15 @@ public class ControllerConfig {
     @Bean
     public BoardController boardController(){
         return new BoardController();
+    }
+
+    @Bean
+    public HomeController homeController(){
+        return new HomeController();
+    }
+
+    @Bean
+    public MyPageController myPageController(){
+        return new MyPageController();
     }
 }
