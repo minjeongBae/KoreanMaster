920ff41cd984a2e7fc7e7a203cde6db36af9ead7
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-12-09 18:49:12 +0900

    feat: 회원가입 정보 db에 저장

--- src/main/java/koreanmaster/user/service/SignUp.java
+++ src/main/java/koreanmaster/user/service/SignUp.java
@@ -1,4 +1,35 @@
 package koreanmaster.user.service;
 
+import koreanmaster.user.student.dao.StudentDAO;
+import koreanmaster.user.student.dto.StudentDTO;
+import koreanmaster.user.teacher.dao.TeacherDAO;
+import koreanmaster.user.teacher.dto.TeacherDTO;
+
 public class SignUp {
+    public boolean studentUser(String email, String pw, String name,
+                               String birth, int level, String phone) {
+        try{
+            StudentDTO student = new StudentDTO(email, pw, name, birth, level, phone);
+            StudentDAO dao = new StudentDAO();
+            dao.addStudent(student);
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean teacherUser(String email, String password, String name,
+                               String birth, boolean qualified, String phone){
+        try{
+            TeacherDTO teacher = new TeacherDTO(email, password, name, birth, qualified, phone);
+            TeacherDAO dao = new TeacherDAO();
+            dao.addTeacher(teacher);
+            return true;
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
