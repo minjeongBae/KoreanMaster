445fbd7d972cb5160ef66fc1f245fafa7819c0e6
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-12-12 01:57:37 +0900

    style: 코드 포매팅

--- src/main/java/koreanmaster/Application.java
+++ src/main/java/koreanmaster/Application.java
@@ -1,13 +1,8 @@
 package koreanmaster;
 
-import koreanmaster.user.dao.UserDAO;
-
-import java.sql.SQLException;
-
 public class Application {
-    public static void main(String[] args) throws SQLException {
-        UserDAO dao = new UserDAO();
-        System.out.println(dao.checkCorrect("ssii620@naver.com","qoalswjd620!"));
+    public static void main(String[] args) {
+
     }
 }
 
