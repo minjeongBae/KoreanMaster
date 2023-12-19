7c726e58da81f2cc64b2c162878c39254ce2f9d2
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2023-12-19 17:15:23 +0900

    refactor: dao 클래스 세분화

--- src/main/java/koreanmaster/mypage/student/dao/Insert.java
+++ src/main/java/koreanmaster/mypage/student/dao/Insert.java
@@ -1,13 +1,12 @@
 package koreanmaster.mypage.student.dao;
 
 import koreanmaster.common.Dao;
-import koreanmaster.common.ExecuteSql;
 import koreanmaster.mypage.student.dto.StudentDTO;
 
 import java.sql.SQLException;
 
-public class StudentDAO extends Dao {
-    public StudentDAO() throws SQLException {}
+public class Insert extends Dao {
+    public Insert() throws SQLException {}
 
     public void addStudent(StudentDTO student) {
         String sql = "INSERT INTO Student VALUES (\"" + student.getEmail() + "\", \""
