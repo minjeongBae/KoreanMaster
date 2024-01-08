f213c45d8da7165cbd81c261cc09d281186d6d41
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2024-01-08 16:37:13 +0900

    feat: mybatis 연동

--- src/main/java/koreanmaster/Application.java
+++ src/main/java/koreanmaster/Application.java
@@ -1,8 +1,29 @@
 package koreanmaster;
 
+import koreanmaster.common.mapper.TimeMapper;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/config/root-context.xml")
 public class Application {
     public static void main(String[] args) {
 
+    }
+
+    @Setter(onMethod_ = @Autowired)
+    private TimeMapper mapper;
+
+    @Test
+    public void getTimeTest() {
+        System.out.println("Now : "+mapper.getTest());
+        log.info("Now : "+mapper.getTest());
     }
 }
 
