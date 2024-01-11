22a82e98fc64503cb43b17cf76b9beb86c079bb7
minjeongBae <80668358+minjeongBae@users.noreply.github.com>
2024-01-11 22:19:02 +0900

    refactor: 불필요한 클래스 삭제

--- src/main/java/koreanmaster/common/mapper/TimeMapper.java
+++
@@ -1,8 +0,0 @@
-package koreanmaster.common.mapper;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface TimeMapper {
-    @Select("SELECT name FROM Teacher WHERE email=\"teacher1@naver.com\";")
-    public String getTest();
-}
